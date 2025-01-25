588acbc252ec11896eefc5e784143e1e9f622b2e
Vidhanjain143 <vidhanjain143@gmail.com>
2025-01-25 18:34:58 +0530

    Updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,13 @@ const About = () => {
             <div className="pb-8">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>    
             </div>
-            <p className="text-xl mt-20">Hi, I'm Vidhan Jain, a final year student at Dayananda Sagar College of Engineering. I'm passionate about building immersive web experiences using technologies like React, HTML, Tailwind CSS, and JavaScript. My journey into the world of technology has led me to explore the realms of Full Stack Development, where I've delved into databases like MongoDB and Firebase for real-time applications. I take pride in my projects, where I've applied these technologies to bring creative ideas to life.</p>
+            <p className="text-xl mt-20">Hi, I'm Vidhan Jain, a final-year Computer Science student at Dayananda Sagar College of Engineering with a passion for crafting intuitive and immersive web experiences. My expertise lies in frontend development, leveraging technologies like React, JavaScript, HTML, and Tailwind CSS to design responsive and user-friendly interfaces.</p>
             <br/>
-            <p className="text-xl">I've been recognized with a <span className='font-bold italic'>Special Mention</span> at a Hackathon conducted by Election Commission of India, a testament to my problem-solving skills and innovative thinking. I thrive on challenges and am always eager to take on new problems. From building interactive frontends to ensuring seamless backend functionality, I enjoy the entire spectrum of development. Frontend development holds a special place in my heart, and I'm enthusiastic about refining user interfaces to create intuitive and visually appealing designs.</p>
+            <p className="text-xl">I have professional experience at <span className='font-semibold'>Hashedin by Deloitte</span>, where I contributed to the development of scalable and user-centric platforms. My work involved building and optimizing frontend architectures using Angular and React, ensuring seamless performance, and delivering features that enhanced application efficiency by up to 20%. I also collaborated across teams to deploy client-ready solutions, resolved critical bugs, and improved user experience through meticulous attention to detail and innovation.</p>
+            <br/>
+            <p className="text-xl">Beyond professional contributions, I've brought creative ideas to life through projects like <span className='font-semibold'>Codewars</span> —a dynamic online coding competition platform—and CodeHub, an advanced coding environment with real-time compilers and performance-focused design.</p>
+            <br/>
+            <p className="text-xl">Recognized with a <span className='font-semibold'>Special Mention</span> at a Hackathon by the <bold>Election Commission of India</bold>, I take pride in my problem-solving skills and innovative mindset. With a keen eye for detail and a deep passion for frontend development, I strive to create seamless, visually appealing designs that enhance user experiences.</p>
           </div>
     </div>
   )
